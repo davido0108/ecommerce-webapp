6d231911f43aa748fbbdb33a9483930e9b11fbb2
Egri David <69672492+davido0108@users.noreply.github.com>
2024-02-07 14:14:37 +0200

    Notification Service added, dockerization started

--- microservices/order-service/src/main/java/com/backend/orderservice/service/OrderService.java
+++ microservices/order-service/src/main/java/com/backend/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.backend.orderservice.service;
 import com.backend.orderservice.dto.InventoryResponse;
 import com.backend.orderservice.dto.OrderLineItemsDTO;
 import com.backend.orderservice.dto.OrderRequest;
+import com.backend.orderservice.event.OrderPlacedEvent;
 import com.backend.orderservice.model.Order;
 import com.backend.orderservice.model.OrderLineItems;
 import com.backend.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -51,6 +54,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             System.out.println("Order Placed");
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
